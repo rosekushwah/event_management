3ff695ff74970f6cafd10b7e19da5def39f54ccc
rosekushwah <kroshni9977@gmail.com>
2025-03-27 16:09:32 +0530

    second change

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-        <h1>Hello</h1>
+    <>
+         <h1>Hello</h1>
+        <h5>Home page</h5>
+    </>
+   
   );
 }
